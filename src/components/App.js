33a62730a014f849e8aaf5d8e21ac340ed74b25e
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-01-16 11:33:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,21 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+
+
     render() {
 
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               FLAMES
+               <input data-testid="input1">First Name</input> {/*Input should be considered case-sensitive */}
+               <input data-testid="input2">Second Name</input>
+               <button data-testid="calculate_relationship">Calculate Relationship Future</button>
+               <h3>
+                {/*Display the output here */}
+               </h3>
+               <button data-testid="clear">Clear Button</button>
             </div>
         )
     }
